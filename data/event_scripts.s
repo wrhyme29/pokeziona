f61a33563dece930a39040b6bf255a303d6537ac
Aliza Mintz <alizamintz@Alizas-MacBook-Pro.local>
2026-06-10 13:44:30 +0300

    wip

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1058,3 +1058,13 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/Kibbutz/scripts.inc"
+
+	.include "data/maps/Kvish1/scripts.inc"
+
+	.include "data/maps/TelAviv/scripts.inc"
+
+	.include "data/maps/Beach/scripts.inc"
+
+	.include "data/maps/Haifa/scripts.inc"
