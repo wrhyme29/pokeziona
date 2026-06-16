514068516b0b33e2ed5257944006a3fc400b8d14
wrhyme29 <wrhyme29@gmail.com>
2026-06-16 18:47:58 +0300

    add fairy type

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -13,7 +13,7 @@ WALLPAPERGFXDIR := graphics/pokemon_storage/wallpapers
 JPCONTESTGFXDIR := graphics/contest/japanese
 TITLESCREENGFXDIR := graphics/title_screen
 
-types := normal fight flying poison ground rock bug ghost steel mystery fire water grass electric psychic ice dragon dark
+types := normal fight flying poison ground rock bug ghost steel mystery fire water grass electric psychic ice dragon dark fairy
 contest_types := cool beauty cute smart tough
 
 
