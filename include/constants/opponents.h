46161c130499ec091f8649df0a22d09acfd5aa88
wrhyme29 <wrhyme29@gmail.com>
2026-07-31 14:55:58 +0000

    add initial trainer

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -4,8 +4,14 @@
 #include "constants/battle_partner.h"
 #include "constants/opponents_frlg.h"
 
+/**
+    Due to a limited amount of trainer space available,
+    we will be overriding existing trainers with our new ones
+    that are in Ziona.
+*/
+
 #define TRAINER_NONE                          0
-#define TRAINER_SAWYER_1                      1
+#define TRAINER_ASFAR_OMAR                    1
 #define TRAINER_GRUNT_AQUA_HIDEOUT_1          2
 #define TRAINER_GRUNT_AQUA_HIDEOUT_2          3
 #define TRAINER_GRUNT_AQUA_HIDEOUT_3          4
